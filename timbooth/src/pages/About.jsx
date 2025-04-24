e184dd500ddae45861b7197f4af4dfdbd93429fa
Timqtzy <timothytenido@gmail.com>
2025-04-24 13:11:34 +0800

    fix about page

--- timbooth/src/pages/About.jsx
+++ timbooth/src/pages/About.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import Navbar from "../component/Navbar";
+import {
+  Camera,
+  Palette,
+  Sparkles,
+  Save,
+  ShieldUser,
+  Cloud,
+  ImageUp,
+} from "lucide-react";
 
 const About = () => {
   return (
@@ -10,50 +19,74 @@ const About = () => {
         </section>
       </div>
 
-      <div className="flex-grow flex flex-col justify-center items-center gap-6  text-justify sm:text-text-left leading-loose px-4">
-        <section className="text-[#2E2F2D] w-full max-w-screen-lg p-4 border border-slate-300 rounded-2xl">
-          <h3 className="font-bold text-xl">Features</h3>
-          <p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 max-w-screen-lg mx-auto mb-14">
+        <section className="text-[#2E2F2D] w-full p-4 border border-slate-300 rounded-2xl h-full">
+          <h3 className="font-bold text-2xl">Features</h3>
+          <p className="py-2">
             Our photo booth website lets you capture, customize, and download
             fun, memorable photos with ease. Whether you're logged in or just
             visiting, you can:
           </p>
-          <p className="font-medium">
-            📸 Take high-quality photos using your webcam
-          </p>
-          <p className="font-medium">
-            🎨 Customize your snapshots with backgrounds, emojis, and text
-          </p>
-          <p className="font-medium">
-            ✨ Automatically remove and replace backgrounds for a creative twist
-          </p>
-          <p className="font-medium">
-            💾 Download your photo or save it to the cloud (for logged-in users)
-          </p>
-          <p>
+          <div className="flex flex-col gap-2">
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <Camera className="w-5 h-5" />
+              </span>{" "}
+              Take high-quality photos using your webcam
+            </p>
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <Palette className="w-5 h-5" />
+              </span>{" "}
+              Customize your snapshots with backgrounds, emojis, and text
+            </p>
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <Sparkles className="w-5 h-5" />
+              </span>{" "}
+              Automatically remove and replace backgrounds for a creative twist
+            </p>
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <Save className="w-5 h-5" />
+              </span>{" "}
+              Download your photo or save it to the cloud (for logged-in users)
+            </p>
+          </div>
+          <p className="py-2">
             Logged-in users get access to extra features like secure image
             saving, gallery management, and cloud storage integration.
           </p>
         </section>
-        <section className="text-[#2E2F2D] w-full max-w-screen-lg p-4 border border-slate-300 rounded-2xl mb-14">
-          <h3 className="font-semibold text-xl">Privacy</h3>
-          <p>We value your privacy. Here's how we handle your data:</p>
-          <p>
-            Guest users' photos are never uploaded — downloads happen locally in
-            your browser.
+        <section className="text-[#2E2F2D] w-full p-4 border border-slate-300 rounded-2xl h-full">
+          <h3 className="font-semibold text-2xl">Privacy</h3>
+          <p className="py-2">
+            We value your privacy. Here's how we handle your data:
           </p>
-          <p className="font-medium">
-            🎨 Logged-in users choose when to upload their images, and these are
-            stored securely in the cloud.
-          </p>
-          <p className="font-medium">
-            ✨ Automatically remove and replace backgrounds for a creative twist
-          </p>
-          <p>
-            No personal data is shared with third parties, and all communication
-            is encrypted for your safety.{" "}
-          </p>
-          <p>You're in control of your content — always.</p>
+          <div className="flex flex-col gap-2">
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <ImageUp className="w-5 h-5" />
+              </span>{" "}
+              Guest users' photos are never uploaded — downloads happen locally
+              in your browser.
+            </p>
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <Cloud className="w-5 h-5" />
+              </span>{" "}
+              Logged-in users choose when to upload their images, and these are
+              stored securely in the cloud.
+            </p>
+            <p className="font-medium flex gap-2">
+              <span className="flex-shrink-0 w-6">
+                <ShieldUser className="w-5 h-5" />
+              </span>{" "}
+              No personal data is shared with third parties, and all
+              communication is encrypted for your safety.
+            </p>
+          </div>
+          <p className="py-2"> You're in control of your content — always.</p>
         </section>
       </div>
     </div>
